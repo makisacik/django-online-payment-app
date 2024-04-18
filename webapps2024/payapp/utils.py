97bb0603de7401a3ee58db0483ba4a618c1fa4f1
makisacik <glsabaton@outlook.com>
2024-04-18 13:19:32 +0100

    Remove currency api and add dynamic url

--- webapps2024/payapp/utils.py
+++ webapps2024/payapp/utils.py
@@ -1,8 +1,15 @@
 import requests
-
+from django.urls import reverse
+from django.conf import settings
 
 def convert_currency(from_currency, to_currency, amount):
-    service_url = f'http://localhost:8000/conversion/{from_currency}/{to_currency}/{amount}/'
+    relative_url = reverse('currency_conversion', args=[from_currency, to_currency, amount])
+
+    scheme = getattr(settings, 'DEFAULT_SCHEME', 'http')
+    domain = getattr(settings, 'DEFAULT_DOMAIN', 'localhost:8000')
+
+    service_url = f'{scheme}://{domain}{relative_url}'
+
     response = requests.get(service_url)
 
     if response.status_code == 200:
